320be412a59cb4e63992cc3f1e75894a952f9187
Stafford Horne <shorne@gmail.com>
2016-09-24 21:08:05 +0900

    Add ignore and simple build script

---
+++ build.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+# Script for building the intiramfs
+#  user arg: clean to clean anything this does
+
+BUSYBOX=$HOME/work/openrisc/busybox/_install
+LIBC=/opt/shorne/software/or1k-musl/or1k-linux-musl/or1k-linux-musl/lib/libc.so
+
+# clean
+if [ "$1" = 'clean' ] ; then
+
+  rm -rf initramfs/bin
+  rm -rf initramfs/sbin
+  rm -rf initramfs/usr/bin
+  rm -rf initramfs/usr/sbin
+  rm -rf initramfs/lib
+  rm initramfs/init
+
+  exit 0
+fi
+
+
+if [ -d $BUSYBOX ] ; then
+ echo "Installing busybox"
+ cp -a $BUSYBOX/* initramfs/
+ 
+ # Init just drops us to the shell
+ echo "Installing init"
+ ln -s sbin/init initramfs/init
+
+else
+ echo "Cannot find busybox install in '$BUSYBOX', aborting"
+ exit 1
+fi
+
+if [ -f $LIBC ] ; then
+ echo "Installing libc"
+ mkdir -p initramfs/lib
+ cp $LIBC initramfs/lib
+else
+ echo "Cannot find libc at '$LIBC' install not completed"
+ exit 1
+fi
+
+exit 0
