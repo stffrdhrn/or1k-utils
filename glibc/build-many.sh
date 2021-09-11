37549eb4c73f3540b299970e9ac94f1b8594ee09
Stafford Horne <shorne@gmail.com>
2021-09-12 06:25:29 +0900

    glibc: Add THREADS for make, add status to email
    Other things include:
     - Add gmp-native, needed by gdb (binutils-native) now
     - Update tests explained, many tests fixed
     - add protobuf.build, for testing compiler/toolchain bugs

--- glibc/build-many.sh
+++ glibc/build-many.sh
@@ -7,20 +7,30 @@ BUILD=${BUILDDIR}/build-many
 SRC=${BUILD}/src/
 
 branch=or1k-glibc-2
+#binutils_branch=mainline
+gcc_branch=releases/gcc-10
+binutils_branch=or1k-gdbserver-nat
 
 if [ "$BUILD_MANY_SETUP" ] ; then
   mkdir -p $SRC
-  [ ! -L $SRC/glibc ]    && ln -s $DIR/glibc $SRC/glibc
-  [ ! -L $SRC/gcc ]      && ln -s $DIR/gcc $SRC/gcc
-  [ ! -L $SRC/binutils ] && ln -s $DIR/binutils-gdb $SRC/binutils
+  [ ! -L $SRC/glibc ]    && ln -s $BUILDDIR/glibc $SRC/glibc
+  [ ! -L $SRC/gcc ]      && ln -s $BUILDDIR/gcc $SRC/gcc
+  [ ! -L $SRC/binutils ] && ln -s $BUILDDIR/binutils-gdb $SRC/binutils
+  #[ ! -L $SRC/gmp ]      && ln -s $BUILDDIR/gmp-6.1.2 $SRC/gmp
+  #[ ! -L $SRC/mpc ]      && ln -s $BUILDDIR/mpc-1.0.3 $SRC/mpc
+  #[ ! -L $SRC/mpfr ]     && ln -s $BUILDDIR/mpfr-3.1.5 $SRC/mpfr
 
   $SRC/glibc/scripts/build-many-glibcs.py $BUILD checkout \
-    gcc-vcs-mainline \
-    binutils-vcs-mainline \
+    gcc-vcs-${gcc_branch} \
+    binutils-vcs-${binutils_branch} \
     glibc-vcs-${branch}
 
   $SRC/glibc/scripts/build-many-glibcs.py $BUILD host-libraries
+
+  $SRC/glibc/scripts/build-many-glibcs.py --keep failed $BUILD compilers i686-gnu i686-gnu
+  #$SRC/glibc/scripts/build-many-glibcs.py --keep failed $BUILD compilers or1k-linux-gnu or1k-linux-gnu-soft
 fi
 
-$SRC/glibc/scripts/build-many-glibcs.py --keep failed $BUILD compilers or1k-linux-gnu or1k-linux-gnu-soft
-$SRC/glibc/scripts/build-many-glibcs.py --keep failed $BUILD glibcs or1k-linux-gnu or1k-linux-gnu-soft
+#$SRC/glibc/scripts/build-many-glibcs.py --keep failed $BUILD glibcs    or1k-linux-gnu or1k-linux-gnu-soft
+
+$SRC/glibc/scripts/build-many-glibcs.py --keep failed $BUILD glibcs    i686-gnu i686-gnu
