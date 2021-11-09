7fb405210232e0977869c81f7f3089900e88b785
Stafford Horne <shorne@gmail.com>
2021-11-10 06:35:51 +0900

    glibc: Add test progress monitoring, fix build-many
    Build-many only works with -O2 that is supposedly documented somewhere,
    but its not clear considering.

--- glibc/build-many.sh
+++ glibc/build-many.sh
@@ -11,6 +11,15 @@ branch=or1k-glibc-2
 gcc_branch=releases/gcc-10
 binutils_branch=or1k-gdbserver-nat
 
+# TODO BUILD_OTHER_COMPILERS
+others="i686-gnu x86_64-linux-gnu riscv32-linux-gnu-rv32imac-ilp32 riscv64-linux-gnu-rv64imac-lp64"
+
+# Glibc needs to be built with -O2 to be clean
+export CFLAGS="-g -O2"
+
+#OTHER=riscv32-linux-gnu-rv32imac-ilp32
+OTHER=x86_64-linux-gnu
+
 if [ "$BUILD_MANY_SETUP" ] ; then
   mkdir -p $SRC
   [ ! -L $SRC/glibc ]    && ln -s $BUILDDIR/glibc $SRC/glibc
@@ -26,11 +35,12 @@ if [ "$BUILD_MANY_SETUP" ] ; then
     glibc-vcs-${branch}
 
   $SRC/glibc/scripts/build-many-glibcs.py $BUILD host-libraries
-
-  $SRC/glibc/scripts/build-many-glibcs.py --keep failed $BUILD compilers i686-gnu i686-gnu
-  #$SRC/glibc/scripts/build-many-glibcs.py --keep failed $BUILD compilers or1k-linux-gnu or1k-linux-gnu-soft
 fi
 
-#$SRC/glibc/scripts/build-many-glibcs.py --keep failed $BUILD glibcs    or1k-linux-gnu or1k-linux-gnu-soft
+if [ "$BUILD_MANY_COMPILERS" ] ; then
+  $SRC/glibc/scripts/build-many-glibcs.py --keep failed $BUILD compilers $OTHER
+  $SRC/glibc/scripts/build-many-glibcs.py --keep failed $BUILD compilers or1k-linux-gnu-soft
+fi
 
-$SRC/glibc/scripts/build-many-glibcs.py --keep failed $BUILD glibcs    i686-gnu i686-gnu
+$SRC/glibc/scripts/build-many-glibcs.py --keep failed $BUILD   glibcs    $OTHER
+$SRC/glibc/scripts/build-many-glibcs.py --keep failed $BUILD   glibcs    or1k-linux-gnu-soft
